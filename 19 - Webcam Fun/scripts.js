02776cd58bed8990ed2535102b8f04b0d983aff6
barnettm <barnett717@gmail.com>
2018-03-12 16:29:47 -0700

    basic paint to canvas complete

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -9,6 +9,32 @@ function getVideo() {
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
         .then(localMediaStream => {
             console.log(localMediaStream)
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play()
+        })
+        .catch(err => {
+            console.error('Oh NO' + err)
         })
 }
+
+function paintToCanvas() {
+    const width = video.videoWidth
+    const height = video.videoHeight
+    canvas.width = width;
+    canvas.height = height
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+}
+
+getVideo()
+
+video.addEventListener('canplay', paintToCanvas)
+
 
