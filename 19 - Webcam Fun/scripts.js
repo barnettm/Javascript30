20fabfb97a93028416067a660df78defd9284419
barnettm <barnett717@gmail.com>
2018-03-12 17:05:56 -0700

    red and split functions complete

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,6 +25,13 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height)
+        // take pixels out
+        let pixels = ctx.getImageData(0,0, width, height);
+        // mess with them
+        // pixels = redEffect(pixels)
+        pixels = rgbSplit(pixels)
+        // put them back
+        ctx.putImageData(pixels, 0,0)
     }, 16);
 }
 
@@ -38,8 +45,28 @@ function takePhoto() {
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'handsome')
-    link.textContent = "Download Image";
+    link.innerHTML = `<img src="${data}" alt = "Handsome Man">`
     strip.insertBefore(link, strip.firstChild)
+}
+
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4){
+        pixels.data[i] = pixels.data[i] + 100 // r
+        pixels.data[i+1] = pixels.data[i+1] -50  //g
+        pixels.data[i+2] = pixels.data[i+2] *0.5  //b
+    }
+    return pixels;
+}
+
+
+function rgbSplit(pixels) {
+    for(let i = 0; i<pixels.data.length; i+=4){
+        pixels.data[i - 150] = pixels.data[i] + 100 // r
+        pixels.data[i + 100] = pixels.data[i+1] -50  //g
+        pixels.data[i - 550] = pixels.data[i+2] *0.5  //b
+    }
+    return pixels;
 }
 
 getVideo()
